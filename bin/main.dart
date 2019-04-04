731a94ee855f98ecb947d19a81b4fad6847ad9ad
sherienebassem@gmali.com <=>
2019-04-04 18:36:51 +0300

    First Project With Dart Language

---
+++ bin/main.dart
@@ -0,0 +1,21 @@
+import 'package:dartProjects/funEXRanTryCa.dart';
+
+main() {
+  int computerGuess = randomComputerGuess();
+  int userInput ;
+  int userValidNumber ;
+  try{
+    do{
+      userInput = askUserInput();
+      userValidNumber = validEX(userInput);
+      var result = compareAnswers(computerGuess,userValidNumber);
+      print(result);
+    }while(computerGuess != userValidNumber);
+  }on FormatException catch(e){
+    print("Enter the number correctly and then restart App");
+  }
+
+
+}
+
+//FormatException
---
+++ lib/funEXRanTryCa.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+import 'dart:io';
+// function (validate input)
+int validEX(int input) {
+  var str = r"^\d{1,2}$";
+  RegExp reg = RegExp(str);
+  if (reg.hasMatch(input.toString())) {
+    return input;
+  } else {
+    return 0;
+  }
+}
+// function (Guess  random number with computer )
+int randomComputerGuess() {
+  Random ran = Random();
+  return ran.nextInt(50);
+}
+// function (Ask user input for Guess Number )
+int askUserInput(){
+  print("Welcome Guess Number Game :");
+  print("Enter Your Guess Number :");
+  String input=stdin.readLineSync();
+  int convertInput=int.parse(input);
+  return convertInput;
+}
+
+Map <bool,String> compareAnswers(int computerGuess , int userGuess){
+  if (userGuess < computerGuess){
+    return {false : "Too Low"};
+  }else if (userGuess > computerGuess){
+    return {false : "Too High"};
+  }else {
+    return {true : "You Guessed it"};
+  }
+}
+
+
+//print("Enter Your Number :");
+//do {
+//userGuess = stdin.readLineSync();
+//int conInput = int.parse(userGuess);
+//if (conInput < computerGuess) {
+//print("Too Low");
+//} else if (conInput > computerGuess) {
+//print("Too High");
+//}
+//
+//} while (computerGuess != userGuess);
+//
+//print("Your Win");
+//
+////======
+//}
